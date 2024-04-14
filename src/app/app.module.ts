9996a91ff9fb78d90f081322e39482291bcdcae8
shin-kinos <kinoshita00000000@gmail.com>
2024-04-14 15:07:41 +0100

    add mat-tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Imported by no-fishi
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule    } from '@angular/material/tabs';
+import { MatIconModule    } from '@angular/material/icon';
+import { MatCardModule    } from '@angular/material/card';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // Imported by no-fishi
+    MatToolbarModule,
+    MatTabsModule,
+    MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
